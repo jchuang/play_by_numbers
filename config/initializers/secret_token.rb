71dc1ef1563af943d909808fde8f8658bb12bed7
Janet Chuang <janet.chuang@gmail.com>
2014-02-12 16:11:45 -0500

    first commit, Rails 4.0.2 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlayByNumbers::Application.config.secret_key_base = '0c0565e2ba750ec225f1c657854e030c9e3381a2d62b47d784d663cdaaa66379fa2943cd7c1b77b15e8c9ef7d7721f2c5385a9fa0f3b8498baa905d3e165b156'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PlayByNumbers::Application.config.session_store :cookie_store, key: '_play_by_numbers_session'
