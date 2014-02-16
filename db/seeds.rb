72285b5750d600a68626c6b6d73b2ce39481c397
Janet Chuang <janet.chuang@gmail.com>
2014-02-16 00:43:24 -0500

    raise exception when record creation fails

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ require 'pry'
 @doc = Nokogiri::XML(File.open(Rails.root + 'db/data/julius_caesar.xml'))
 
 play_title = @doc.xpath('/PLAY/TITLE').text
-Play.find_or_create_by(title: play_title)
+Play.find_or_create_by!(title: play_title)
 play = Play.find_by(title: play_title)
 
 act_nodes = @doc.xpath('//ACT/TITLE')
 act_nodes.each do |act_node|
   act_title = act_node.text
-  Act.find_or_create_by(title: act_title, play_id: play.id)
+  Act.find_or_create_by!(title: act_title, play_id: play.id)
 end
