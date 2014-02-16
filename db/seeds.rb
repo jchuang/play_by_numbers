be9f26c96c1eb34f81ba341e32a1f7c5c4304ff3
Janet Chuang <janet.chuang@gmail.com>
2014-02-16 17:05:31 -0500

    seeder creates scene records

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,14 +21,19 @@ speakers.each do |speaker|
   Speaker.find_or_create_by!(name: name, play_id: play.id)
 end
 
-acts = @doc.xpath('//ACT')
-acts.each do |act|
-  act_title = act.xpath('./TITLE').text
-  Act.find_or_create_by!(title: act_title, play_id: play.id)
+act_nodes = @doc.xpath('//ACT')
+act_nodes.each do |act_node|
 
-  scenes = act.xpath('./SCENE')
-  scenes.each do |scene|
-    scene_title = scene.xpath('./TITLE').text
+  act_title = act_node.xpath('./TITLE').text
+  Act.find_or_create_by!(title: act_title, play_id: play.id)
+  act = Act.find_by(title: act_title, play_id: play.id)
+
+  scene_nodes = act_node.xpath('./SCENE')
+  scene_nodes.each do |scene_node|
+
+    scene_title = scene_node.xpath('./TITLE').text
     Scene.find_or_create_by!(title: scene_title, act_id: act.id)
+    scene = Scene.find_by(title: scene_title, act_id: act.id)
+
   end
 end
