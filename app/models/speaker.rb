5be878e682e3a65f9dd2ed58c222de768f3e0831
Janet Chuang <janet.chuang@gmail.com>
2014-02-18 18:49:08 -0500

    working methods for counting lines and scenes

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -9,7 +9,7 @@ class Speaker < ActiveRecord::Base
   has_many :lines, through: :speeches
 
   def num_lines
-    'num_lines'
+    lines.count
   end
 
   def longest_speech_lines
@@ -17,11 +17,13 @@ class Speaker < ActiveRecord::Base
   end
 
   def num_scenes
-    'num_scenes'
+    scenes.count
   end
 
+  # This should be handled elsewhere.  A play knows its total number of scenes.
+  # Rails is using a cached value for num_scenes.
   def percent_scenes
-    'percent_scenes'
+    ((num_scenes * 100.0) / 18).round
   end
 
 end
