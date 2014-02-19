09352d9977d3bda0b374f14ab8dd4f5481e7745b
Janet Chuang <janet.chuang@gmail.com>
2014-02-19 17:51:30 -0500

    limit to first 10 results

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,7 +6,7 @@ class PlaysController < ApplicationController
 
   def show
     @play = Play.find(params[:id])
-    @speakers = Speaker.where(play_id: params[:id]).sort_speakers(params)
+    @speakers = Speaker.where(play_id: params[:id]).sort_speakers(params)[0..9]
     @total_scenes = @play.scenes.count
   end
 
