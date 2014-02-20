790bf7624922a0bc958efc9412b211588a08d3d6
Janet Chuang <janet.chuang@gmail.com>
2014-02-19 20:43:52 -0500

    add direction to column sort method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
 
   def column_sort(column_title, method_name)
-    link_to column_title, sort: method_name
+    direction = (params[:sort] == method_name && params[:direction] == "desc") ? "asc" : "desc"
+    link_to column_title, sort: method_name, direction: direction
   end
 
 end
