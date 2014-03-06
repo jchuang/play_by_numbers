1320d9c76ae96c2a011bc7cfd35e0d234148b54d
Janet Chuang <janet.chuang@gmail.com>
2014-03-05 22:00:41 -0500

    seeds.rb is more concise, still works correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,7 @@ require 'nokogiri'
 document = Nokogiri::XML(File.open(Rails.root + 'db/data/julius_caesar.xml'))
 
 play_title = document.xpath('/PLAY/TITLE').text
-Play.find_or_create_by!(title: play_title)
-play = Play.find_by(title: play_title)
+play = Play.find_or_create_by!(title: play_title)
 
 speakers = document.xpath('//PERSONA')
 speakers.each do |speaker|
@@ -51,27 +50,19 @@ act_nodes = document.xpath('//ACT')
 act_nodes.each do |act_node|
 
   act_title = act_node.xpath('./TITLE').text
-  Act.find_or_create_by!(title: act_title, play_id: play.id)
-  act = Act.find_by(title: act_title, play_id: play.id)
+  act = Act.find_or_create_by!(title: act_title, play_id: play.id)
 
   scene_nodes = act_node.xpath('./SCENE')
   scene_nodes.each do |scene_node|
 
     scene_title = scene_node.xpath('./TITLE').text
-    Scene.find_or_create_by!(title: scene_title, act_id: act.id)
-    scene = Scene.find_by(title: scene_title, act_id: act.id)
+    scene = Scene.find_or_create_by!(title: scene_title, act_id: act.id)
 
     speech_nodes = scene_node.xpath('./SPEECH')
     speech_nodes.each do |speech_node|
 
       speaker_nodes = speech_node.xpath('./SPEAKER')
-      if speaker_nodes.count > 1
-        speaker_nodes.each { |speaker_node| create_speech(speaker_node, speech_node, scene) }
-
-      else
-        speaker_node = speaker_nodes.first
-        create_speech(speaker_node, speech_node, scene)
-      end
+      speaker_nodes.each { |speaker_node| create_speech(speaker_node, speech_node, scene) }
     end
   end
 end
